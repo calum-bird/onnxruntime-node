fcecf99918226ebf058439609131d39cb59435b5
Calum Bird <calum@trelent.net>
2023-08-29 19:47:09 -0700

    delete the rest

--- onnxruntime/core/providers/vitisai/include/vaip/global_api.h
+++
@@ -1,7 +0,0 @@
-
-// Copyright (c) 2023 Advanced Micro Devices, Inc. All rights reserved.
-// Licensed under the MIT License.
-#pragma once
-#include "core/session/onnxruntime_cxx_api.h"
-
-std::vector<OrtCustomOpDomain*> initialize_vitisai_ep();
